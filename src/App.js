9f0f3f3dc34c854a37cfe873f1a9c5a22350b01b
dwideas <115578606+dwideas@users.noreply.github.com>
2025-06-28 12:10:06 -0500

    testing 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500 text-white text-4xl">
-      Hello testing
+      Hello testing 
     </div>
   );
 }
